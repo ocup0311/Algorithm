d4fb2d521c8317f7f9ef46ccb7a0c30f94233aa8
Ocup <ocup0311@gmail.com>
2021-11-13 16:17:05 +0800

    [UPDATE] honei

--- honei.js
+++ honei.js
@@ -1,19 +1,36 @@
 // ===================== main =====================
 
-function honei(num, first, second, third) {
-  let step = [0, 1, 3, 7, 15]
+function honei(num, _first, _second, _third) {
+  let step = 0
+  const [first, second, third] = [_first, _second, _third]
 
-  // 發想：
-  // 有空 1.就從Ｆ拿去非空 2.再空
-  // 沒空 1.
+  const doHonei = (n, F, S, T) => {
+    if (n === 1) {
+      T.push(F.pop())
+      step++
+    } else {
+      doHonei(n - 1, F, T, S)
+      doHonei(1, F, S, T)
+      doHonei(n - 1, S, F, T)
+    }
+  }
 
-  // 1大＋順序小([8,7,6,5][4][3,2,1])：開始套用 honei
-  // 套用完再加1即可再套用。
-  // 結案。
+  doHonei(num, first, second, third)
 
-  return step[num]
+  return step
 }
 
 // ===================== test =====================
 
-console.log(honei(2, [2, 1], [], []))
+console.log(honei(10, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], [], []))
+
+console.log(2 ** 10 - 1)
+
+// ===================== 發想 =====================
+
+// 有空 1.就從Ｆ拿去非空 2.再空
+// 沒空 1.
+
+// 1大＋順序小([8,7,6,5][4][3,2,1])：開始套用 honei
+// 套用完再加1即可再套用。
+// 結案。
