2fcec0daf2d6b9e9d1b540fb1ea0e0353e9aa1ba
Ocup <ocup0311@gmail.com>
2021-11-25 13:41:27 +0800

    [MOVE] permutation

---
+++ permutation/heap.js
@@ -0,0 +1,16 @@
+// by B. R. Heap
+// ===================== main =====================
+
+const input1 = ['A', 'B', 'C']
+const input2 = ['A', 'B', 'C', 'D']
+const input3 = ['A', 'B', 'C', 'D', 'E']
+
+function heap(arr) {
+  return
+}
+
+// ===================== test =====================
+
+console.log(`======= ${input1} =======`, '\n', heap(input1))
+console.log(`======= ${input2} =======`, '\n', heap(input2))
+console.log(`======= ${input3} =======`, '\n', heap(input3))
