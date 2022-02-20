1dff7b33cb0dc43c06f80d0b8c3ce2d43a08910e
Ocup <ocup0311@gmail.com>
2022-02-20 19:33:42 +0800

    [ADD] functions: LinkedList new methods

--- code/linkedList/LinkedList.js
+++ code/linkedList/LinkedList.js
@@ -11,30 +11,140 @@ class LinkedList {
     this.length = 0
   }
 
-  push(value) {
+  findNode(index) {
+    let currentNode = this.head
+
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next
+    }
+
+    return currentNode
+  }
+
+  findLastNode() {
+    return this.findNode(this.length - 1)
+  }
+
+  traverse(indexOfEnd, callBack) {
     if (this.head === null) {
-      this.head = new Node(value)
+      console.log('This is an empty list.')
+      return
+    }
+
+    let currentNode = this.head
+    for (let i = 0; i <= indexOfEnd; i++) {
+      callBack(currentNode)
+      currentNode = currentNode.next
+    }
+  }
+
+  traverseAll(callBack) {
+    this.traverse(this.length - 1, callBack)
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+
+    if (this.head === null) {
+      this.head = newNode
     } else {
-      let currentNode = this.head
-      while (currentNode.next !== null) {
-        currentNode = currentNode.next
-      }
-      currentNode.next = new Node(value)
+      const lastNode = this.findLastNode()
+      lastNode.next = newNode
     }
 
     this.length++
+
+    return this.length
+  }
+
+  pop() {
+    // exception
+    if (this.head === null) {
+      console.log('This is an empty list.')
+      return
+    }
+
+    // run
+    const last2Node = this.findNode(this.length - 2)
+    const popNode = last2Node.next
+    last2Node.next = null
+
+    this.length--
+
+    return popNode.value
+  }
+
+  shift(value) {
+    const newNode = new Node(value)
+
+    if (this.head !== null) newNode.next = this.head
+
+    this.head = newNode
+    this.length++
+
+    return this.length
+  }
+
+  unshift() {
+    // exception
+    if (this.head === null) {
+      console.log('This is an empty list.')
+      return
+    }
+
+    // run
+    const unshiftNode = this.head
+    this.head = unshiftNode.next
+    unshiftNode.next = null
+
+    this.length--
+
+    return unshiftNode.value
+  }
+
+  inserAt(index, value) {
+    const newNode = new Node(value)
+
+    if (this.head === null) {
+      this.head = newNode
+    } else {
+      const preNode = this.findNode(index - 1)
+      newNode.next = preNode.next
+      preNode.next = newNode
+    }
+
+    this.length++
+
+    return this.length
+  }
+
+  removeAt(index) {
+    // exception
+    if (this.head === null) {
+      console.log('This is an empty list.')
+      return
+    }
+
+    // run
+    const preNode = this.findNode(index - 1)
+    const removedNode = preNode.next
+    preNode.next = removedNode.next
+    removedNode.next = null
+
+    this.length--
+
+    return removedNode.value
+  }
+
+  getValue(index) {
+    return this.findNode(index).value
   }
 
   printAll() {
-    // exception
-    if (this.head === null) console.log('This is an empty list.')
+    // function
+    const callBack = (currentNode) => console.log(currentNode.value)
 
-    // run
-    let currentNode = this.head
-    while (currentNode !== null) {
-      console.log(currentNode.value)
-      currentNode = currentNode.next
-    }
+    this.traverseAll(callBack)
   }
 }
 
--- code/linkedList/index.js
+++ code/linkedList/index.js
@@ -1,8 +1,16 @@
 import LinkedList from './LinkedList.js'
 
 const testList1 = new LinkedList()
-testList1.push(1)
-testList1.push(2)
-testList1.push(3)
-testList1.push(4)
+const a = testList1.push(1)
+const b = testList1.push(2)
+const c = testList1.push(3)
 testList1.printAll()
+const gg = testList1.pop()
+const d = testList1.push(4)
+console.log('size: ', a, b, c, d)
+console.log('gg:', gg)
+
+testList1.printAll()
+const vv = testList1.getValue(1)
+
+console.log('vv:', vv)
