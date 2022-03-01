9bc9f545206a5121c8fba4714c1f9dfc2bb8fdaf
Ocup <ocup0311@gmail.com>
2022-03-01 09:17:10 +0800

    [ADD] util: exception

--- code/@util/errorHelpers/index.js
+++ code/@util/errorHelpers/index.js
@@ -2,9 +2,15 @@ import OwnError from './OwnError.js'
 
 export { default as OwnError } from './OwnError.js'
 
+// [TODO] exception useful or not or how to design?
+export const exception = ({ message = '', result = null, error = false }) => {
+  if (error) throw new OwnError(message)
+  if (message) console.log(message)
+  return result
+}
+
 export const getObjValue = (object, property) => {
   if (object.hasOwnProperty(property)) return object[property]
-
   throw new OwnError(
     `Please use any in [ ${Object.keys(object)} ] instead of "${property}"`
   )
