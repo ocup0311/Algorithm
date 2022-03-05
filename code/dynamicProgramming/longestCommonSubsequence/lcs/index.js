7e869ecb583552715b3ce81dcf71e78e9905ed30
Ocup <ocup0311@gmail.com>
2022-03-05 13:16:45 +0800

    [ADD] file: dynamicProgramming/lcs/method3

--- code/dynamicProgramming/longestCommonSubsequence/lcs/index.js
+++ code/dynamicProgramming/longestCommonSubsequence/lcs/index.js
@@ -1,2 +1,3 @@
 export { default as method1 } from './method1.js'
 export { default as method2 } from './method2.js'
+export { default as method3 } from './method3.js'
---
+++ code/dynamicProgramming/longestCommonSubsequence/lcs/method3.js
@@ -0,0 +1,76 @@
+const lcs = (_str1, _str2) => {
+  const [str1, str2] = [' ' + _str1, ' ' + _str2]
+  const [rowSize, columnSize] = [str1.length, str2.length]
+  const table = new Array(rowSize)
+  let [ptrR, ptrC] = [1, 1]
+  let step = 0
+
+  // function
+  const initialTable = () => {
+    const initialItem = { length: 0, previous: null }
+
+    table[0] = new Array(columnSize).fill(initialItem)
+
+    for (let i = 1; i < columnSize; i++) {
+      const newArr = new Array(columnSize)
+      newArr[0] = initialItem
+      table[i] = newArr
+    }
+  }
+
+  const fillTable = () => {
+    step++
+
+    // exception
+    if (str1[ptrR] === str2[ptrC]) {
+      const previous = table[ptrR - 1][ptrC - 1]
+
+      table[ptrR][ptrC] = {
+        length: previous.length + 1,
+        previous,
+        same: str1[ptrR],
+      }
+      return
+    }
+
+    // run
+    const up = table[ptrR - 1][ptrC]
+    const left = table[ptrR][ptrC - 1]
+    let previous
+
+    if (up.length > left.length) previous = up
+    else previous = left
+
+    table[ptrR][ptrC] = { length: previous.length, previous }
+  }
+
+  const buildResult = () => {
+    let ptr = table[rowSize - 1][columnSize - 1]
+    const length = ptr.length
+    let result = ''
+
+    while (ptr !== null) {
+      if (ptr.same) result = ptr.same + result
+      ptr = ptr.previous
+    }
+
+    return { result, length, step }
+  }
+
+  // run
+  initialTable()
+
+  while (ptrR < rowSize) {
+    while (ptrC < columnSize) {
+      fillTable()
+      ptrC++
+    }
+
+    ptrR++
+    ptrC = 1
+  }
+
+  return buildResult()
+}
+
+export default lcs
