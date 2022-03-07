5bb680d140ab4851e46f81410e8d0c90d628e829
Ocup <ocup0311@gmail.com>
2022-03-07 17:22:23 +0800

    [CLEAN] dynamicProgramming/01KnapsackProblem

--- code/dynamicProgramming/01KnapsackProblem/Node.js
+++ code/dynamicProgramming/01KnapsackProblem/Node.js
@@ -1,12 +1,20 @@
 class Node {
-  constructor(cost, profit, lowerBound, upperBound, layer = 0, prev = null) {
+  constructor({
+    cost,
+    profit,
+    lowerBound,
+    upperBound,
+    layer = 0,
+    prev = null,
+    addedItem = null,
+  }) {
     this.cost = cost
     this.profit = profit
     this.lowerBound = lowerBound
     this.upperBound = upperBound
     this.layer = layer
     this.prev = prev
-    this.addedItem = null
+    this.addedItem = addedItem
   }
 }
 
--- code/dynamicProgramming/01KnapsackProblem/zeroOneKP.js
+++ code/dynamicProgramming/01KnapsackProblem/zeroOneKP.js
@@ -4,129 +4,139 @@ import PriorityQueue from '../../tree/PriorityQueue.js'
 import Node from './Node.js'
 
 const zeroOneKP = ({ items, maxCost }) => {
-  const output = []
-  const restItems = items
+  // var
+  let lowerBound_global = 0
+  const candidates = new PriorityQueue('max')
+  const sortedItems = items
     .map((v) => ({ ...v, cp: v.profit / v.cost }))
     .sort((a, b) => a.cp - b.cp)
-  const candidates = new PriorityQueue('max')
-  const finalcandidates = new PriorityQueue('max')
-  let lowerBound_tree = 0
 
   // function
   const toEnqueue = (node) => {
     candidates.enqueue(node, 'upperBound', false)
   }
+
   const toDequeue = () => candidates.dequeue()
 
-  const makeBound = (cost = 0, profit = 0, layer = 0) => {
-    let lastItem = null
-    let upperBound = undefined
-    let currentCost = cost
-    let lowerBound = profit
+  const makeBound = (cost, profit, layer) => {
+    // function
+    const makeLowerBound = () => {
+      let lastItem = null
+      let lowerBound = profit
+      let currentCost = cost
 
-    for (let i = restItems.length - 1 - layer; i > -1; i--) {
-      lastItem = restItems[i]
-      currentCost = currentCost + lastItem.cost
+      for (let i = sortedItems.length - 1 - layer; i > -1; i--) {
+        const currentItem = sortedItems[i]
+        currentCost = currentCost + currentItem.cost
 
-      if (currentCost > maxCost) break
+        if (currentCost > maxCost) {
+          lastItem = currentItem
+          break
+        }
 
-      lowerBound = lowerBound + lastItem.profit
-      lastItem = null
+        lowerBound = lowerBound + currentItem.profit
+      }
+
+      return [lastItem, lowerBound, currentCost]
     }
 
-    if (lastItem) {
-      const cc = maxCost - (currentCost - lastItem.cost)
+    const makeUpperBound = (lastItem, lowerBound, currentCost) => {
+      if (!lastItem) return lowerBound
 
-      upperBound = lowerBound + lastItem.cp * cc
+      const fractionalCost = maxCost - (currentCost - lastItem.cost)
+      return lowerBound + lastItem.cp * fractionalCost
     }
 
-    return [lowerBound, upperBound ?? lowerBound]
+    // run
+    const [lastItem, lowerBound, currentCost] = makeLowerBound()
+
+    const upperBound = makeUpperBound(lastItem, lowerBound, currentCost)
+
+    return [lowerBound, upperBound]
   }
 
   const updateglobalBound = (lowerBound) => {
-    if (lowerBound > lowerBound_tree) lowerBound_tree = lowerBound
+    if (lowerBound > lowerBound_global) lowerBound_global = lowerBound
   }
 
-  const makeOutput = () => {
-    let bestcandidates = finalcandidates.dequeue()
+  const addNode = ({
+    cost,
+    profit,
+    lowerBound,
+    upperBound,
+    layer,
+    prev,
+    addedItem,
+  }) => {
+    const newNode = new Node({
+      cost,
+      profit,
+      lowerBound,
+      upperBound,
+      layer,
+      prev,
+      addedItem,
+    })
+    updateglobalBound(lowerBound)
+    toEnqueue(newNode)
+    return newNode
+  }
 
-    while (bestcandidates !== null) {
-      if (bestcandidates.addedItem) output.push(bestcandidates.addedItem)
-      bestcandidates = bestcandidates.prev
+  const initialize = () => {
+    const [lowerBound, upperBound] = makeBound(0, 0, 0)
+    addNode({ cost: 0, profit: 0, lowerBound, upperBound })
+  }
+
+  const makeCandidate = ({ prev, addedItem }) => {
+    const cost = prev.cost + (addedItem ? addedItem.cost : 0)
+    const profit = prev.profit + (addedItem ? addedItem.profit : 0)
+    const layer = prev.layer + 1
+    const [lowerBound, upperBound] = makeBound(cost, profit, layer)
+
+    if (upperBound >= lowerBound_global && cost <= maxCost) {
+      return addNode({
+        cost,
+        profit,
+        lowerBound,
+        upperBound,
+        layer,
+        prev,
+        addedItem,
+      })
+    }
+  }
+
+  const makeOutput = (currentNode) => {
+    const output = []
+
+    while (currentNode !== null) {
+      if (currentNode.addedItem) output.push(currentNode.addedItem)
+      currentNode = currentNode.prev
+    }
+
+    return output
+  }
+
+  const run = () => {
+    while (candidates.queue.length > 0) {
+      const prevNode = toDequeue()
+      const prevLayer = prevNode.layer
+      const currentItem = sortedItems[sortedItems.length - 1 - prevLayer]
+
+      if (prevLayer === items.length) return makeOutput(prevNode)
+
+      if (prevNode.upperBound < lowerBound_global) break
+
+      makeCandidate({ prev: prevNode })
+
+      makeCandidate({ prev: prevNode, addedItem: currentItem })
     }
   }
 
   // run
-  const [lowerBound_F, upperBound_F] = makeBound()
-  const firtNode = new Node(0, 0, lowerBound_F, upperBound_F)
-  updateglobalBound(lowerBound_F)
-  toEnqueue(firtNode)
+  initialize()
 
-  while (candidates.queue.length > 0) {
-    const prevNode = toDequeue()
-    const prevLayer = prevNode.layer
-    const currentItem = restItems[restItems.length - 1 - prevLayer]
-
-    if (prevLayer === items.length) {
-      finalcandidates.enqueue(prevNode, 'lowerBound', false)
-      continue
-    }
-
-    if (prevNode?.upperBound < lowerBound_tree) break
-
-    const cost_R = prevNode.cost
-    const profit_R = prevNode.profit
-    const [lowerBound_R, upperBound_R] = makeBound(
-      cost_R,
-      profit_R,
-      prevLayer + 1
-    )
-
-    const cost_L = currentItem.cost + prevNode.cost
-    const profit_L = currentItem.profit + prevNode.profit
-    const [lowerBound_L, upperBound_L] = makeBound(
-      cost_L,
-      profit_L,
-      prevLayer + 1
-    )
-
-    const maxBound_R = Math.max(
-      upperBound_R,
-      cost_L <= maxCost && lowerBound_L,
-      lowerBound_tree
-    )
-    const maxBound_L = Math.max(upperBound_L, lowerBound_R, lowerBound_tree)
-
-    if (upperBound_R === maxBound_R) {
-      const newNode_R = new Node(
-        cost_R,
-        profit_R,
-        lowerBound_R,
-        upperBound_R,
-        prevLayer + 1,
-        prevNode
-      )
-
-      updateglobalBound(lowerBound_R)
-      toEnqueue(newNode_R)
-    }
-
-    if (upperBound_L === maxBound_L && cost_L <= maxCost) {
-      const newNode_L = new Node(
-        cost_L,
-        profit_L,
-        lowerBound_L,
-        upperBound_L,
-        prevLayer + 1,
-        prevNode
-      )
-      newNode_L.addedItem = currentItem
-      updateglobalBound(lowerBound_L)
-      toEnqueue(newNode_L)
-    }
-  }
-
-  makeOutput()
+  const output = run()
 
   return output
 }
