b25a28f6ab058f1370ebc7f6b0b4cbad4e3cda4a
Ocup <ocup0311@gmail.com>
2022-03-21 16:15:44 +0800

    [UPDATE] util: deepClone

--- code/@util/index.js
+++ code/@util/index.js
@@ -14,4 +14,28 @@ export const toDownCounter = (counter, item) => {
 // other
 export const openLog = (v) => console.log(JSON.stringify(v, null, ' '))
 
-export const deepClone = (data) => JSON.parse(JSON.stringify(data))
+// export const deepClone = (data) => JSON.parse(JSON.stringify(data))
+// --> There're some limits: Symbol key, undefined value, cyclic object
+export const deepClone = (value, cache = new WeakMap()) => {
+  // exception
+  if (value === null || typeof value !== 'object') return value
+  if (value instanceof Date || value instanceof RegExp)
+    return value.constructor(value)
+  if (cache.has(value)) return cache.get(value)
+
+  // var
+  const clone = value.constructor()
+  const allKeys = [
+    ...Object.getOwnPropertyNames(value),
+    ...Object.getOwnPropertySymbols(value),
+  ]
+
+  // run
+  cache.set(value, clone)
+
+  allKeys.forEach((key) => {
+    clone[key] = deepClone(value[key], cache)
+  })
+
+  return clone
+}
