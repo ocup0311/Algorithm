59a11d0d74c214e3549d281a97c50b35be2e2308
Ocup <ocup0311@gmail.com>
2022-04-19 21:18:48 +0800

    [UPDATE][LEETCODE][String] #2_reverseInteger

--- code/String/LeetCode/#2_reverseInteger.js
+++ code/String/LeetCode/#2_reverseInteger.js
@@ -27,22 +27,28 @@
 // Runtime: 96.02% / 68 ms
 // Memory: 79.62% / 43.7 MB
 const reverse1 = (x) => {
+  const INT_MAX = 2 ** 30 - 1 + 2 ** 30
+  const INT_MIN = -(2 ** 30) - 2 ** 30
+
   const strX = x.toString()
   const arrX = strX.split('')
 
   const reStrX = arrX.reduceRight((t, v) => (v === '-' ? v + t : t + v), '')
   const reNumX = Number(reStrX)
 
-  if (-(2 ** 31) > reNumX || reNumX > 2 ** 31 - 1) return 0
+  if (INT_MIN > reNumX || reNumX > INT_MAX) return 0
 
   return reNumX
 }
 
 // 2. ------------------------------------------------------------
 // 以不能顯示超過範圍的數字為前提
-// Runtime: 27.18% / 118 ms
-// Memory: 60.22% / 44 MB
-const reverse = (x) => {
+// Runtime: 86.44% / 74 ms
+// Memory: 79.62% / 43.8 MB
+const reverse2 = (x) => {
+  const INT_MAX = 2 ** 30 - 1 + 2 ** 30
+  const INT_MIN = -(2 ** 30) - 2 ** 30
+
   // function
   const checkRange = (num) => {
     const strX = num.toString()
@@ -68,10 +74,7 @@ const reverse = (x) => {
     }
 
     // run
-    const checkResult =
-      num < 0
-        ? check(arrX, -(2 ** 30) - 2 ** 30)
-        : check(arrX, 2 ** 30 - 1 + 2 ** 30)
+    const checkResult = num < 0 ? check(arrX, INT_MIN) : check(arrX, INT_MAX)
 
     return checkResult && arrX
   }
@@ -85,4 +88,74 @@ const reverse = (x) => {
   const reNumX = Number(reStrX)
 
   return reNumX
+}
+
+// 3. ------------------------------------------------------------
+// just number
+// Runtime: 78.36% / 79 ms
+// Memory: 79.62% / 43.7 MB
+const reverse3 = (x) => {
+  // function
+  const getRest = (n) => Math.trunc(n / 10)
+  const getPop = (n) => n % 10
+
+  // var
+  const INT_MAX = 2 ** 30 - 1 + 2 ** 30
+  const INT_MIN = -(2 ** 30) - 2 ** 30
+  const PRE_MAX = getRest(INT_MAX)
+  const PRE_MIN = getRest(INT_MIN)
+  const END_MAX = getPop(INT_MAX)
+  const END_MIN = getPop(INT_MIN)
+
+  let rest = x
+  let reX = 0
+
+  // run
+  while (rest !== 0) {
+    const pop = getPop(rest)
+    rest = getRest(rest)
+
+    if (reX > PRE_MAX || (reX === PRE_MAX && pop > END_MAX)) return 0
+    if (reX < PRE_MIN || (reX === PRE_MIN && pop < END_MIN)) return 0
+
+    reX = reX * 10 + pop
+  }
+
+  return reX
+}
+
+// 4. ------------------------------------------------------------
+// Runtime: 92.76% / 70 ms
+// Memory: 49.55% / 44 MB
+const reverse = (x) => {
+  // CONST
+  const INT_MAX = 2 ** 30 - 1 + 2 ** 30
+  const INT_MIN = -(2 ** 30) - 2 ** 30
+
+  // function
+  const makeArr = (num) => Math.abs(num).toString().split('')
+
+  // var
+  const arrX = makeArr(x)
+  const lenX = arrX.length
+  const sign = x < 0 ? '-' : '+'
+  const arrLimit = x < 0 ? makeArr(INT_MIN) : makeArr(INT_MAX)
+  const lenLimit = arrLimit.length
+
+  let reStrX = ''
+  let isValid = lenX < lenLimit ? true : false
+
+  // run
+  for (let i = 0; i < lenX; i++) {
+    const pop = arrX[lenX - 1 - i]
+
+    if (!isValid) {
+      if (pop < arrLimit[i]) isValid = true
+      if (pop > arrLimit[i]) return 0
+    }
+
+    reStrX = reStrX + pop
+  }
+
+  return Number(sign + reStrX)
 }
