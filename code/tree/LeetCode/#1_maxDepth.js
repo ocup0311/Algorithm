bc7a9950fb1d3ee4eed78ddc52ec45a46ec3f5c8
Ocup <ocup0311@gmail.com>
2022-04-21 21:03:34 +0800

    [UPDATE][LEETCODE][TREE] #1_maxDepth

--- code/tree/LeetCode/#1_maxDepth.js
+++ code/tree/LeetCode/#1_maxDepth.js
@@ -38,10 +38,12 @@
 // 1. ------------------------------------------------------------
 // Runtime: 72.09% / 77 ms
 // Memory: 67.53% / 45 MB
-const maxDepth = (root) => {
+const maxDepth1 = (root) => {
+  // var
   let max = 0
   let tmp = 0
 
+  // function
   const dfs = (node) => {
     tmp++
 
@@ -57,6 +59,90 @@ const maxDepth = (root) => {
     tmp--
   }
 
+  // run
+  dfs(root)
+
+  return max
+}
+
+// 2. ------------------------------------------------------------
+const maxDepth2 = (root) => {
+  // var
+  let max = 0
+  let tmp = 0
+
+  // function
+  const dfs = (node) => {
+    if (node === null) return
+
+    tmp++
+    dfs(node.left)
+    dfs(node.right)
+
+    if (tmp > max) max = tmp
+    tmp--
+  }
+
+  // run
+  dfs(root)
+
+  return max
+}
+
+// 3. ------------------------------------------------------------
+const maxDepth3 = (root) => {
+  // var
+  let max = 0
+  let tmp = 0
+
+  // function
+  const callFn = (fn) => typeof fn === 'function' && fn()
+
+  const dfs = (node, { preOrder, inOrder, postOrder }) => {
+    if (node === null) return
+
+    callFn(preOrder)
+    dfs(node.left, { preOrder, inOrder, postOrder })
+    callFn(inOrder)
+    dfs(node.right, { preOrder, inOrder, postOrder })
+    callFn(postOrder)
+  }
+
+  const nodeIn = () => {
+    tmp++
+    if (tmp > max) max = tmp
+  }
+
+  const nodeOut = () => {
+    tmp--
+  }
+
+  // run
+  dfs(root, { preOrder: nodeIn, postOrder: nodeOut })
+
+  return max
+}
+
+// 4. ------------------------------------------------------------
+const maxDepth4 = (root) => {
+  // var
+  let max = 0
+  let tmp = 0
+
+  // function
+  const dfs = (node) => {
+    if (node !== null) {
+      tmp++
+      if (tmp > max) max = tmp
+
+      dfs(node.left)
+      dfs(node.right)
+
+      tmp--
+    }
+  }
+
+  // run
   dfs(root)
 
   return max
