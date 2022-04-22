5bf326717cf0167a624128e974819951d5528d21
Ocup <ocup0311@gmail.com>
2022-04-22 14:32:34 +0800

    [ADD][LEETCODE][TREE] #5_sortedArrayToBST

---
+++ code/tree/LeetCode/#5_sortedArrayToBST.js
@@ -0,0 +1,55 @@
+// Given an integer array nums where the elements are sorted in ascending order,
+// convert it to a height-balanced binary search tree.
+
+// A height-balanced binary tree is a binary tree in which the depth of the two
+// subtrees of every node never differs by more than one.
+
+// Example 1:
+// Input: nums = [-10,-3,0,5,9]
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+// Example 2:
+// Input: nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+// Constraints:
+// 1 <= nums.length <= 10^4
+// -10^4 <= nums[i] <= 10^4
+// nums is sorted in a strictly increasing order.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+
+// Notice --------------------------------------------------------
+// 1. 高度平衡
+// 2. 已排序的 Array
+
+// 1. ------------------------------------------------------------
+// Runtime: 78.39% / 79 ms
+// Memory: 91.29% / 44.2 MB
+const sortedArrayToBST = (nums, idxS = 0, idxE = nums.length - 1) => {
+  if (idxS > idxE) return null
+
+  const idxM = Math.ceil((idxS + idxE) / 2)
+  const root = new TreeNode(nums[idxM])
+
+  const left = sortedArrayToBST(nums, idxS, idxM - 1)
+  const right = sortedArrayToBST(nums, idxM + 1, idxE)
+
+  root.left = left
+  root.right = right
+
+  return root
+}
