4e25a6ba468057e872ece2baeb6e08b8d283134c
Ocup <ocup0311@gmail.com>
2022-04-24 09:00:34 +0800

    [ADD][LEETCODE][Math] #1_climbStairs

---
+++ code/Math/LeetCode/#1_climbStairs.js
@@ -0,0 +1,59 @@
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+// Example 1:
+// Input: n = 3
+// Output: ["1","2","Fizz"]
+
+// Example 2:
+// Input: n = 5
+// Output: ["1","2","Fizz","4","Buzz"]
+// Example 3:
+
+// Input: n = 15
+// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+// Constraints:
+// 1 <= n <= 10＾4
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+// Notice --------------------------------------------------------
+// 1. index from 1
+
+// 1. ------------------------------------------------------------
+// Runtime: 80.37% / 72 ms
+// Memory: 96.29% / 43.9 MB
+const fizzBuzz = (n) => {
+  const result = new Array(n)
+
+  for (let i = 1; i <= n; i++) {
+    const idx = i - 1
+
+    if (i % 15 === 0) {
+      result[idx] = 'FizzBuzz'
+      continue
+    }
+
+    if (i % 3 === 0) {
+      result[idx] = 'Fizz'
+      continue
+    }
+
+    if (i % 5 === 0) {
+      result[idx] = 'Buzz'
+      continue
+    }
+
+    result[idx] = `${i}`
+  }
+
+  return result
+}
