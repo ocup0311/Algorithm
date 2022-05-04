43cb3afc0f24e13ece7bd10c2d039638d6e29554
Ocup <ocup0311@gmail.com>
2022-05-04 10:45:47 +0800

    [ADD][LEETCODE][LINKEDLIST][MEDIUM] #2_oddEvenList

---
+++ code/linkedList/LeetCode/medium/#2_oddEvenList.js
@@ -0,0 +1,61 @@
+// Given the head of a singly linked list, group all the nodes with odd indices together
+// followed by the nodes with even indices, and return the reordered list.
+
+// The first node is considered odd, and the second node is even, and so on.
+
+// Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+// You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [1,3,5,2,4]
+
+// Example 2:
+// Input: head = [2,1,3,5,6,4,7]
+// Output: [2,3,6,7,1,5,4]
+
+// Constraints:
+// The number of nodes in the linked list is in the range [0, 104].
+// -10^6 <= Node.val <= 10^6
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// Notice --------------------------------------------------------
+// 1. O(1) extra space & O(n) time
+// 2. 可能 head === null
+
+// 1. ------------------------------------------------------------
+// Runtime: 76.21% / 80 ms
+// Memory Usage: 93.87% / 44.2 MB
+const oddEvenList = (head) => {
+  // exception
+  if (!head?.next?.next) return head
+
+  // var
+  const even = head.next
+  let ptrO = head
+  let ptrE = even
+
+  // run
+  while (ptrE?.next) {
+    ptrO.next = ptrE.next
+    ptrO = ptrO.next
+    ptrE.next = ptrO.next
+    ptrE = ptrE.next
+  }
+
+  ptrO.next = even
+
+  return head
+}
