fed44cfcd03c91272abbc1e373685c94f8989e65
Ocup <ocup0311@gmail.com>
2022-05-05 15:31:59 +0800

    [ADD][LEETCODE][TREE][MEDIUM] #4_connect

---
+++ code/tree/LeetCode/medium/#4_connect.js
@@ -0,0 +1,153 @@
+// You are given a perfect binary tree where all leaves are on the same level, and every
+// parent has two children. The binary tree has the following definition:
+
+// struct Node {
+//   int val;
+//   Node *left;
+//   Node *right;
+//   Node *next;
+// }
+// Populate each next pointer to point to its next right node. If there is no next right node,
+// the next pointer should be set to NULL.
+
+// Initially, all next pointers are set to NULL.
+
+// Example 1:
+// Input: root = [1,2,3,4,5,6,7]
+// Output: [1,#,2,3,#,4,5,6,7,#]
+// Explanation: Given the above perfect binary tree (Figure A), your function should populate
+// each next pointer to point to its next right node, just like in Figure B. The serialized
+// output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+// Example 2:
+// Input: root = []
+// Output: []
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 2^12 - 1].
+// -1000 <= Node.val <= 1000
+
+// Follow-up:
+// You may only use constant extra space.
+// The recursive approach is fine. You may assume implicit stack space does not count as
+// extra space for this problem.
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+
+// Notice --------------------------------------------------------
+
+// 1. ------------------------------------------------------------
+// Runtime: 10.02% / 144 ms
+// Memory Usage: 0% / 50.2 MB
+const connect1 = (root) => {
+  if (!root) return root
+
+  const connectLevel = (level) => {
+    level.reduceRight((nextNode, currNode) => {
+      currNode.next = nextNode
+      return currNode
+    }, null)
+
+    const newLevel = level.reduce((nextLevel, node) => {
+      const newNodes = [node.left, node.right].filter((v) => v)
+      if (newNodes[0]) return nextLevel.concat(newNodes)
+      return nextLevel
+    }, [])
+
+    if (newLevel[0]) connectLevel(newLevel)
+  }
+
+  connectLevel([root])
+
+  return root
+}
+
+// 2. ------------------------------------------------------------
+// Runtime: 96 ms
+// Memory Usage: 48.4 MB
+const connect2 = (root) => {
+  if (!root) return root
+
+  const connectLevel = (level) => {
+    level.reduceRight((nextNode, currNode) => {
+      currNode.next = nextNode
+      return currNode
+    }, null)
+
+    const newLevel = level.reduce((nextLevel, node) => {
+      if (node.left) nextLevel.push(node.left)
+      if (node.right) nextLevel.push(node.right)
+
+      return nextLevel
+    }, [])
+
+    if (newLevel[0]) connectLevel(newLevel)
+  }
+
+  connectLevel([root])
+
+  return root
+}
+
+// 3. ------------------------------------------------------------
+// use stack & loop
+// Runtime: 94 ms
+// Memory Usage: 49.6 MB
+const connect3 = (root) => {
+  if (!root) return root
+
+  const queue = [[root, 1]]
+  const hashMap = new Map()
+
+  while (queue.length > 0) {
+    const [node, level] = queue.shift()
+
+    if (hashMap.has(level)) hashMap.get(level).next = node
+    hashMap.set(level, node)
+
+    if (node.left) queue.push([node.left, level + 1])
+    if (node.right) queue.push([node.right, level + 1])
+  }
+
+  return root
+}
+
+// 4. ------------------------------------------------------------
+// use stack & loop
+// change: hashMap --> one node
+// Runtime: 80 ms
+// Memory Usage: 49.5 MB
+const connect = (root) => {
+  if (!root?.left && !root?.right) return root
+
+  const queue = [{ node: root, level: 1 }]
+  let lastNode = { node: null, level: 0 }
+  let ptr = 0
+
+  while (queue.length > ptr) {
+    const { node, level } = queue[ptr]
+
+    if (lastNode.level === level) lastNode.node.next = node
+    lastNode = { node, level }
+
+    if (node.left) queue.push({ node: node.left, level: level + 1 })
+    if (node.right) queue.push({ node: node.right, level: level + 1 })
+
+    ptr++
+  }
+
+  return root
+}
