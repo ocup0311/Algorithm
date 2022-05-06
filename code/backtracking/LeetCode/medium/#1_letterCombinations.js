16fe54f04958cd1c2e1ef87b5bcfee33729e93c4
Ocup <ocup0311@gmail.com>
2022-05-06 11:19:16 +0800

    [UPDATE][LEETCODE][BACKTRACKING][MEDIUM] #1_letterCombinations

--- code/backtracking/LeetCode/medium/#1_letterCombinations.js
+++ code/backtracking/LeetCode/medium/#1_letterCombinations.js
@@ -61,7 +61,7 @@ const letterCombinations1 = (digits) => {
 // 2. ------------------------------------------------------------
 // Runtime: 108 ms
 // Memory Usage: 41.8 MB
-const letterCombinations = (digits) => {
+const letterCombinations2 = (digits) => {
   // exception
   if (!digits) return []
 
@@ -102,4 +102,93 @@ const letterCombinations = (digits) => {
       metadata[ptr].ptr--
     }
   }
+}
+
+// 3. ------------------------------------------------------------
+// Runtime: 62 ms
+// Memory Usage: 41.7 MB
+const letterCombinations3 = (digits) => {
+  // exception
+  if (!digits) return []
+
+  // var
+  const pair = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  }
+  const firstStr = digits.split('').reduce((s, n) => s + pair[n][0], '')
+  const output = [firstStr]
+  let ptr = 0
+
+  // run
+  while (ptr < digits.length) {
+    const options = pair[digits[ptr]]
+    const lenOption = options.length
+    const lenOutput = output.length
+
+    for (let i = 1; i < lenOption; i++) {
+      for (let j = 0; j < lenOutput; j++) {
+        const newStr =
+          output[j].slice(0, ptr) + options[i] + output[j].slice(ptr + 1)
+        output.push(newStr)
+      }
+    }
+
+    ptr++
+  }
+
+  return output
+}
+
+// 4. ------------------------------------------------------------
+// 先算好數量
+// Runtime: 65 ms
+// Memory Usage: 42 MB
+const letterCombinations = (digits) => {
+  // exception
+  if (!digits) return []
+
+  // var
+  const pair = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  }
+  const inputArr = digits.split('')
+  const length = inputArr.reduce((t, n) => t * pair[n].length, 1)
+  const output = new Array(length)
+  output[0] = inputArr.reduce((s, n) => s + pair[n][0], '')
+
+  let ptrD = 0
+  let ptrO = 1
+
+  // run
+  while (ptrD < digits.length) {
+    const options = pair[digits[ptrD]]
+    const lenOption = options.length
+    const lenOutput = ptrO
+
+    for (let i = 1; i < lenOption; i++) {
+      for (let j = 0; j < lenOutput; j++) {
+        output[ptrO] =
+          output[j].slice(0, ptrD) + options[i] + output[j].slice(ptrD + 1)
+        ptrO++
+      }
+    }
+
+    ptrD++
+  }
+
+  return output
 }
