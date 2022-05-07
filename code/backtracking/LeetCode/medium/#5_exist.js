66f07590354b824cfd7674e8e93e0f327b3ab969
Ocup <ocup0311@gmail.com>
2022-05-07 17:04:35 +0800

    [UPDATE][LEETCODE][BACKTRACKING][MEDIUM] #5_exist

--- code/backtracking/LeetCode/medium/#5_exist.js
+++ code/backtracking/LeetCode/medium/#5_exist.js
@@ -93,7 +93,7 @@ const exist1 = (board, word) => {
 // 2. ------------------------------------------------------------
 // Runtime: 1732 ms
 // Memory Usage: 50.4 MB
-const exist = (board, word) => {
+const exist2 = (board, word) => {
   // var
   const store = new Map()
   const lenRow = board.length
@@ -135,6 +135,103 @@ const exist = (board, word) => {
   for (let row = 0; row < lenRow; row++) {
     for (let col = 0; col < lenCol; col++) {
       if (check(row, col)) return true
+    }
+  }
+
+  return false
+}
+
+// 3. ------------------------------------------------------------
+// XXXXX fail
+// 不能存後續結果  <-- 因為 track 不同。
+const exist3 = (board, word) => {
+  //   // var
+  //   const store = {}
+  //   const track = new Map()
+  //   const lenRow = board.length
+  //   const lenCol = board[0].length
+  //   // function
+  //   const isValid = (row, col) => {
+  //     return (
+  //       !track.get(`${row}#${col}`) &&
+  //       row > -1 &&
+  //       row < lenRow &&
+  //       col > -1 &&
+  //       col < lenCol
+  //     )
+  //   }
+  //   const check = (row, col, ptr = 0) => {
+  //     const key = `${row}#${col}`
+  //     if (store[key]?.[ptr] !== undefined) return store[key][ptr]
+  //     if (board[row][col] !== word[ptr]) {
+  //       store[key] ? (store[key][ptr] = false) : (store[key] = { [ptr]: false })
+  //       return false
+  //     }
+  //     if (ptr === word.length - 1) return true
+  //     track.set(key, true)
+  //     const cadidates = [
+  //       [row - 1, col],
+  //       [row + 1, col],
+  //       [row, col - 1],
+  //       [row, col + 1],
+  //     ]
+  //     const result = cadidates.some(
+  //       (cell) => isValid(cell[0], cell[1]) && check(cell[0], cell[1], ptr + 1)
+  //     )
+  //     track.set(key, false)
+  //     store[key] ? (store[key][ptr] = result) : (store[key] = { [ptr]: result })
+  //     return result
+  //   }
+  //   // run
+  //   for (let row = 0; row < lenRow; row++) {
+  //     for (let col = 0; col < lenCol; col++) {
+  //       if (check(row, col)) return true
+  //     }
+  //   }
+  //   return false
+}
+
+// 4. ------------------------------------------------------------
+// 直接改 input
+// Runtime: 67.20% / 513 ms
+// Memory Usage: 5.94% / 50.5 MB
+const exist4 = (board, word) => {
+  // var
+  const lenRow = board.length
+  const lenCol = board[0].length
+
+  // function
+  const isValid = (B, r, c) =>
+    r > -1 && r < lenRow && c > -1 && c < lenCol && B[r][c]
+
+  const check = (B, W, row, col, ptr = 0) => {
+    if (B[row][col] !== W[ptr]) return false
+    if (ptr === W.length - 1) return true
+
+    B[row][col] = null
+
+    const cadidates = [
+      [row - 1, col],
+      [row + 1, col],
+      [row, col - 1],
+      [row, col + 1],
+    ]
+
+    const checkCadidates = (B, W, p) => {
+      return ([r, c]) => isValid(B, r, c) && check(B, W, r, c, p + 1)
+    }
+
+    if (cadidates.some(checkCadidates(B, W, ptr))) return true
+
+    B[row][col] = W[ptr]
+
+    return false
+  }
+
+  // run
+  for (let row = 0; row < lenRow; row++) {
+    for (let col = 0; col < lenCol; col++) {
+      if (check(board, word, row, col)) return true
     }
   }
 
