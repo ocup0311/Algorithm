f6473edd8c26b48d13b689d38c533163704f5c92
Ocup <ocup0311@gmail.com>
2022-05-07 16:01:30 +0800

    [ADD][LEETCODE][BACKTRACKING][MEDIUM] #5_exist

---
+++ code/backtracking/LeetCode/medium/#5_exist.js
@@ -0,0 +1,142 @@
+// Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+// The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are
+// horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+// Example 1:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// Output: true
+
+// Example 2:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+// Output: true
+
+// Example 3:
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+// Output: false
+
+// Constraints:
+// m == board.length
+// n = board[i].length
+// 1 <= m, n <= 6
+// 1 <= word.length <= 15
+// board and word consists of only lowercase and uppercase English letters.
+
+// Follow up: Could you use search pruning to make your solution faster with a larger board?
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+
+// Notice --------------------------------------------------------
+// 1. 水平或垂直相鄰者
+// 2. 一個 cell 只能用一次
+// 3. 只有大小寫英文 --> 大小寫是否是為相同？
+
+// 1. ------------------------------------------------------------
+// Runtime: 3627 ms
+// Memory Usage: 49.4 MB
+const exist1 = (board, word) => {
+  const store = new Map()
+  const lenRow = board.length
+  const lenCol = board[0].length
+
+  const isValid = (row, col) => {
+    return (
+      !store.get(`${row}#${col}`) &&
+      row > -1 &&
+      row < lenRow &&
+      col > -1 &&
+      col < lenCol
+    )
+  }
+  const check = (row, col, ptr = 0) => {
+    if (board[row][col] !== word[ptr]) return false
+    if (ptr === word.length - 1) return true
+
+    store.set(`${row}#${col}`, true)
+
+    let result = false
+
+    if (isValid(row - 1, col)) {
+      result = result || check(row - 1, col, ptr + 1)
+      store.set(`${row - 1}#${col}`, false)
+    }
+    if (isValid(row + 1, col)) {
+      result = result || check(row + 1, col, ptr + 1)
+      store.set(`${row + 1}#${col}`, false)
+    }
+    if (isValid(row, col - 1)) {
+      result = result || check(row, col - 1, ptr + 1)
+      store.set(`${row}#${col - 1}`, false)
+    }
+    if (isValid(row, col + 1)) {
+      result = result || check(row, col + 1, ptr + 1)
+      store.set(`${row}#${col + 1}`, false)
+    }
+
+    return result
+  }
+
+  for (let row = 0; row < lenRow; row++) {
+    for (let col = 0; col < lenCol; col++) {
+      if (check(row, col)) return true
+      store.clear()
+    }
+  }
+
+  return false
+}
+
+// 2. ------------------------------------------------------------
+// Runtime: 1732 ms
+// Memory Usage: 50.4 MB
+const exist = (board, word) => {
+  // var
+  const store = new Map()
+  const lenRow = board.length
+  const lenCol = board[0].length
+
+  // function
+  const isValid = (row, col) => {
+    return (
+      !store.get(`${row}#${col}`) &&
+      row > -1 &&
+      row < lenRow &&
+      col > -1 &&
+      col < lenCol
+    )
+  }
+  const check = (row, col, ptr = 0) => {
+    if (board[row][col] !== word[ptr]) return false
+    if (ptr === word.length - 1) return true
+
+    store.set(`${row}#${col}`, true)
+
+    const cadidates = [
+      [row - 1, col],
+      [row + 1, col],
+      [row, col - 1],
+      [row, col + 1],
+    ]
+
+    const result = cadidates.some(
+      (cell) => isValid(cell[0], cell[1]) && check(cell[0], cell[1], ptr + 1)
+    )
+
+    store.set(`${row}#${col}`, false)
+
+    return result
+  }
+
+  // run
+  for (let row = 0; row < lenRow; row++) {
+    for (let col = 0; col < lenCol; col++) {
+      if (check(row, col)) return true
+    }
+  }
+
+  return false
+}
