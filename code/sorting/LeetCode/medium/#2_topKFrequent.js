9a1f9540328f2aa5c0b7e791295bae8d8f43530d
Ocup <ocup0311@gmail.com>
2022-05-09 16:29:44 +0800

    [UPDATE][LEETCODE][SORT][MEDIUM] #2_topKFrequent

--- code/sorting/LeetCode/medium/#2_topKFrequent.js
+++ code/sorting/LeetCode/medium/#2_topKFrequent.js
@@ -31,7 +31,7 @@
 // 1. ------------------------------------------------------------
 // Runtime: 88 ms
 // Memory Usage: 44.4 MB
-const topKFrequent = (nums, k) => {
+const topKFrequent1 = (nums, k) => {
   const count = nums.reduce((count, v) => {
     if (count[v]) count[v]++
     else count[v] = 1
@@ -42,4 +42,65 @@ const topKFrequent = (nums, k) => {
     .sort((a, b) => b[1] - a[1])
     .slice(0, k)
     .map((v) => v[0])
+}
+
+// 2. ------------------------------------------------------------
+// Runtime: 102 ms
+// Memory Usage: 46.8 MB
+const topKFrequent2 = (nums, k) => {
+  // function
+  const makeFrequent = (count, v) => {
+    const { byNum, byFreq } = count
+    if (byNum[v]) byNum[v]++
+    else byNum[v] = 1
+
+    delete byFreq[byNum[v] - 1]?.[v]
+    if (byFreq[byNum[v]]) byFreq[byNum[v]][v] = true
+    else byFreq[byNum[v]] = { [v]: true }
+
+    return count
+  }
+
+  // run
+  const frequent = nums.reduce(makeFrequent, { byNum: {}, byFreq: [] }).byFreq
+  const result = []
+  let restK = k
+
+  while (restK > 0) {
+    const topFreqArr = Object.keys(frequent.pop())
+
+    restK = restK - topFreqArr.length
+    topFreqArr.forEach((v) => result.push(v))
+  }
+
+  return result
+}
+
+// 3. ------------------------------------------------------------
+// Runtime: 76 ms
+// Memory Usage: 45.3 MB
+const topKFrequent = (nums, k) => {
+  const bucket = new Array(nums.length + 1)
+  const count = nums.reduce((count, v) => {
+    if (count[v]) count[v]++
+    else count[v] = 1
+    return count
+  }, {})
+
+  Object.keys(count).forEach((v) => {
+    bucket[count[v]] ? bucket[count[v]].push(v) : (bucket[count[v]] = [v])
+  })
+
+  const result = []
+  const frequent = bucket.filter((v) => v)
+  let restK = k
+
+  while (restK > 0) {
+    const topFreqArr = frequent.pop()
+
+    restK = restK - topFreqArr.length
+    topFreqArr.forEach((v) => result.push(v))
+  }
+
+  return result
 }
