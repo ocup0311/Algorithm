df0432de0c8823c317dc18513415116687d32ef2
Ocup <ocup0311@gmail.com>
2022-05-14 17:03:38 +0800

    [UPDATE][LEETCODE][DP][MEDIUM] #2_uniquePaths

--- code/dynamicProgramming/LeetCode/medium/#2_uniquePaths.js
+++ code/dynamicProgramming/LeetCode/medium/#2_uniquePaths.js
@@ -29,6 +29,7 @@
 
 // 1. ------------------------------------------------------------
 // 排列組合解
+// 箭頭有：往下 m-1 個，往右 n-1 個。總共 m+n-2 個。m+n-2 個箭頭中，取 m-1 個往下。 --> 組合：(m+n-2) 取 (m-1)
 // Runtime: 90.01% / 62 ms
 // Memory Usage: 86.16% / 41.9 MB
 const Factorial = (() => {
@@ -53,4 +54,78 @@ const uniquePaths1 = (m, n) => {
   const rightP = Factorial(rightN)
 
   return totalP / (downP * rightP)
+}
+
+// 2. ------------------------------------------------------------
+// from 1：不 cache 每次重算
+// Runtime: 60 ms
+// Memory Usage: 41.6 MB
+const uniquePaths2 = (m, n) => {
+  // var
+  const downN = m - 1
+  const rightN = n - 1
+  const totalN = downN + rightN
+
+  // function
+  const Factorial = (n) => {
+    if ((n === 0) | (n === 1)) return 1
+
+    return n * Factorial(n - 1)
+  }
+
+  // run
+  const totalP = Factorial(totalN)
+  const downP = Factorial(downN)
+  const rightP = Factorial(rightN)
+
+  return totalP / (downP * rightP)
+}
+
+// 3. ------------------------------------------------------------
+// 第一排＆第一列，每格都只有一種方式抵達。
+// 第 [i + 1][j + 1] 格，只有兩種方式抵達：(1)從 [i][j + 1] 來 (2) 從 [i + 1][j] 來
+// -->  DP[i + 1][j + 1] = DP[i][j + 1] + DP[i + 1][j]
+// Runtime: 72 ms
+// Memory Usage: 41.9 MB
+const uniquePaths3 = (m, n) => {
+  // exception
+  if (m === 1 || n === 1) return 1
+
+  // var
+  const matrix = new Array(m).fill(new Array(n).fill(1))
+
+  // run
+  for (let ptrR = 1; ptrR < m; ptrR++) {
+    const preRow = matrix[ptrR - 1]
+    const newRow = new Array(n).fill(1)
+
+    for (let ptrC = 1; ptrC < n; ptrC++) {
+      newRow[ptrC] = preRow[ptrC] + newRow[ptrC - 1]
+    }
+
+    matrix[ptrR] = newRow
+  }
+
+  return matrix[m - 1][n - 1]
+}
+
+// 4. ------------------------------------------------------------
+// 可以不留前面的 data
+// Runtime: 65 ms
+// Memory Usage: 41.4 MB
+const uniquePaths4 = (m, n) => {
+  // exception
+  if (m === 1 || n === 1) return 1
+
+  // var
+  const data = Array(m).fill(1)
+
+  // run
+  for (let ptrR = 1; ptrR < n; ptrR++) {
+    for (let ptrC = 1; ptrC < m; ptrC++) {
+      data[ptrC] = data[ptrC] + data[ptrC - 1]
+    }
+  }
+
+  return data[m - 1]
 }
