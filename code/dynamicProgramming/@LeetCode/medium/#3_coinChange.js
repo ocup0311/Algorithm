6716012320b5ec7dc9ac0de9ac9473a669b0d167
Ocup <ocup0311@gmail.com>
2022-05-16 16:55:52 +0800

    [ADD][LEETCODE][DP][MEDIUM] #3_coinChange

--- code/dynamicProgramming/@LeetCode/medium/#3_coinChange.js
+++ code/dynamicProgramming/@LeetCode/medium/#3_coinChange.js
@@ -35,3 +35,114 @@
 // 2. coins 是否已排序？ --> 沒排序
 
 // 1. ------------------------------------------------------------
+// fail: 可能 "大" 少一點，"中" 多一點，則 "小" 可以更少
+// 93 / 188 test cases passed.
+// Input: [186,419,83,408], 6249
+// Output: 26
+// Expected: 20
+const coinChange1 = (coins, amount) => {
+  // exception
+  if (amount === 0) return 0
+
+  // var
+  const sortedCoins = [...coins].sort((a, b) => b - a)
+
+  // function
+  const makeCombination = (prevA, ptrC) => {
+    const coin = sortedCoins[ptrC]
+    let postA = prevA % coin
+    let prevN = Math.floor(prevA / coin)
+
+    if (postA === 0) return prevN
+    if (ptrC === sortedCoins.length - 1) return -1
+
+    while (prevN >= 0) {
+      const postN = makeCombination(postA, ptrC + 1)
+
+      if (postN > 0) return prevN + postN
+
+      postA = postA + coin
+      prevN--
+    }
+
+    return -1
+  }
+
+  // run
+  return makeCombination(amount, 0)
+}
+
+// 2. ------------------------------------------------------------
+// Runtime: 61.85% / 153 ms
+// Memory Usage: 61.07% / 46.8 MB
+const coinChange2 = (coins, amount, count = new Array(amount + 1).fill(0)) => {
+  // exception
+  if (amount < 0) return -1
+  if (amount === 0 || count[amount]) return count[amount]
+
+  // var
+  let tmp = Infinity
+
+  // run
+  for (let coin of coins) {
+    const result = coinChange2(coins, amount - coin, count)
+
+    if (0 <= result && result < tmp) tmp = result + 1
+  }
+
+  count[amount] = tmp === Infinity ? -1 : tmp
+
+  return count[amount]
+}
+
+// 3. ------------------------------------------------------------
+// Runtime: 123 ms
+// Memory Usage: 45.9 MB
+const coinChange3 = (coins, amount) => {
+  // exception
+  if (amount === 0) return 0
+
+  // var
+  const count = new Array(amount + 1).fill(Infinity)
+  count[0] = 0
+
+  // run
+  for (let curAmount = 1; curAmount <= amount; curAmount++) {
+    for (let coin of coins) {
+      if (curAmount < coin) continue
+
+      const preAmount = curAmount - coin
+      count[curAmount] = Math.min(count[curAmount], count[preAmount] + 1)
+    }
+  }
+
+  return count[amount] === Infinity ? -1 : count[amount]
+}
+
+// 4. ------------------------------------------------------------
+// Runtime: 164 ms
+// Memory Usage: 46.2 MB
+const coinChange4 = (coins, amount) => {
+  // exception
+  if (amount === 0) return 0
+
+  // var
+  const count = new Array(amount + 1).fill(Infinity)
+  count[0] = 0
+
+  // run
+  for (let curAmount = 1; curAmount <= amount; curAmount++) {
+    for (let coin of coins) {
+      if (curAmount < coin) continue
+      if (curAmount === coin) {
+        count[curAmount] = 1
+        break
+      }
+
+      const preAmount = curAmount - coin
+      count[curAmount] = Math.min(count[curAmount], count[preAmount] + 1)
+    }
+  }
+
+  return count[amount] === Infinity ? -1 : count[amount]
+}
