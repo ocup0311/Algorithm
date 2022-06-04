2a30a3224b05623c0c25c5d2dec315fe005c42df
Ocup <ocup0311@gmail.com>
2022-06-04 18:32:18 +0800

    [UPDATE][LEETCODE][ARRAY][HARD] #2_spiralOrder

--- code/Array/@LeetCode/hard/#2_spiralOrder.js
+++ code/Array/@LeetCode/hard/#2_spiralOrder.js
@@ -24,7 +24,7 @@
 // 1. ------------------------------------------------------------
 // Runtime: 27.91% / 89 ms
 // Memory Usage: 49.81% / 41.9 MB
-const spiralOrder = (matrix) => {
+const spiralOrder1 = (matrix) => {
   const output = []
   let [top, bottom, left, right] = [-1, matrix.length, -1, matrix[0].length]
   let [ptrR, ptrC] = [0, 0]
@@ -85,4 +85,149 @@ const spiralOrder = (matrix) => {
     if (ptrR === top) return output
     goTop()
   }
+}
+
+// 2. ------------------------------------------------------------
+// new Array first
+// Runtime: 83.62% / 63 ms
+// Memory Usage: 84.24% / 41.7 MB
+const spiralOrder2 = (matrix) => {
+  const [numR, numC] = [matrix.length, matrix[0].length]
+  const output = new Array(numR * numC)
+
+  let [top, bottom, left, right] = [-1, numR, -1, numC]
+  let [ptrR, ptrC, ptrO] = [0, 0, 0]
+
+  // function
+  const goRight = () => {
+    while (ptrC < right) {
+      output[ptrO] = matrix[ptrR][ptrC]
+      ptrO++
+      ptrC++
+    }
+
+    ptrR++
+    ptrC--
+    top++
+  }
+  const goBottom = () => {
+    while (ptrR < bottom) {
+      output[ptrO] = matrix[ptrR][ptrC]
+      ptrO++
+      ptrR++
+    }
+
+    ptrC--
+    ptrR--
+    right--
+  }
+  const goLeft = () => {
+    while (ptrC > left) {
+      output[ptrO] = matrix[ptrR][ptrC]
+      ptrO++
+      ptrC--
+    }
+
+    ptrR--
+    ptrC++
+    bottom--
+  }
+  const goTop = () => {
+    while (ptrR > top) {
+      output[ptrO] = matrix[ptrR][ptrC]
+      ptrO++
+      ptrR--
+    }
+
+    ptrC++
+    ptrR++
+    left++
+  }
+
+  // run
+  while (true) {
+    if (ptrO === output.length) return output
+    goRight()
+
+    if (ptrO === output.length) return output
+    goBottom()
+
+    if (ptrO === output.length) return output
+    goLeft()
+
+    if (ptrO === output.length) return output
+    goTop()
+  }
+}
+
+// 3. ------------------------------------------------------------
+// clean up
+// Runtime: 66 ms
+// Memory Usage: 42.1 MB
+const spiralOrder = (matrix) => {
+  const [numR, numC] = [matrix.length, matrix[0].length]
+  const output = new Array(numR * numC)
+
+  const border = { top: 0, bottom: numR, left: -1, right: numC }
+  let [ptrR, ptrC, ptrO] = [0, 0, 0]
+  let direction = 'right'
+
+  // function
+  const goto = {
+    right: () => {
+      while (ptrC < border[direction]) {
+        output[ptrO] = matrix[ptrR][ptrC]
+        ptrO++
+        ptrC++
+      }
+
+      ptrR++
+      ptrC--
+      border[direction]--
+      direction = 'bottom'
+    },
+    bottom: () => {
+      while (ptrR < border[direction]) {
+        output[ptrO] = matrix[ptrR][ptrC]
+        ptrO++
+        ptrR++
+      }
+
+      ptrC--
+      ptrR--
+      border[direction]--
+      direction = 'left'
+    },
+    left: () => {
+      while (ptrC > border[direction]) {
+        output[ptrO] = matrix[ptrR][ptrC]
+        ptrO++
+        ptrC--
+      }
+
+      ptrR--
+      ptrC++
+      border[direction]++
+      direction = 'top'
+    },
+    top: () => {
+      while (ptrR > border[direction]) {
+        output[ptrO] = matrix[ptrR][ptrC]
+        ptrO++
+        ptrR--
+      }
+
+      ptrC++
+      ptrR++
+      border[direction]++
+      direction = 'right'
+    },
+  }
+
+  // run
+  while (ptrO < output.length) {
+    goto[direction]()
+  }
+
+  return output
 }
