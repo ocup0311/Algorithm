90c5681307ffcac654d454fb0dee86d62d384e36
Ocup <ocup0311@gmail.com>
2022-06-10 10:38:11 +0800

    [UPDATE][LEETCODE][ARRAY][HARD] #9_calculate

--- code/Array/@LeetCode/hard/#9_calculate.js
+++ code/Array/@LeetCode/hard/#9_calculate.js
@@ -37,20 +37,20 @@
 // 2. 有空白
 
 // 1. ------------------------------------------------------------
+// 方法：轉成 postOrder 再運算
 // Runtime: 32.48% / 128 ms
 // Memory Usage: 11.12% / 54 MB
-const calculate = (s) => {
+const calculate1 = (s) => {
   // var
   const inOrder = []
   const postOrder = []
-  const operatorStack = []
-  const outputStack = []
+  const stack1 = []
+  const stack2 = []
 
   // function
   const isOperator = (x) => x === '+' || x === '-' || x === '*' || x === '/'
   const isSpace = (x) => x === ' '
-  const isHigh = (x) => x === '*' || x === '/'
-  const isLow = (x) => x === '+' || x === '-'
+  const isHighPrecedence = (x) => x === '*' || x === '/'
   const cal = (right, left, operator) => {
     right = parseInt(right)
     left = parseInt(left)
@@ -64,24 +64,24 @@ const calculate = (s) => {
   // run
   // 1. inOrder
   for (let i = 0; i < s.length; i++) {
-    let str = s[i]
+    let token = s[i]
 
-    if (isOperator(str)) {
-      inOrder.push(str)
+    if (isSpace(token)) continue
+    if (isOperator(token)) {
+      inOrder.push(token)
       continue
     }
-    if (isSpace(str)) continue
 
     for (let j = i + 1; j < s.length; j++) {
-      const str2 = s[j]
+      const char = s[j]
 
-      if (isOperator(str2) || isSpace(str2)) break
+      if (isOperator(char) || isSpace(char)) break
 
-      str = str + str2
+      token = token + char
       i++
     }
 
-    inOrder.push(str)
+    inOrder.push(token)
   }
 
   // 2. postOrder
@@ -91,37 +91,84 @@ const calculate = (s) => {
       continue
     }
 
-    const opt = operatorStack[operatorStack.length - 1]
+    const opt = stack1[stack1.length - 1]
 
     if (opt) {
-      if (isHigh(token) && isHigh(opt)) postOrder.push(operatorStack.pop())
+      if (isHighPrecedence(token))
+        isHighPrecedence(opt) && postOrder.push(stack1.pop())
+      else {
+        let opt2 = stack1.pop()
 
-      if (isLow(token)) {
-        let opt2 = operatorStack.pop()
-
-        while (isHigh(opt2)) {
+        while (isHighPrecedence(opt2)) {
           postOrder.push(opt2)
-          opt2 = operatorStack.pop()
+          opt2 = stack1.pop()
         }
 
         if (opt2) postOrder.push(opt2)
       }
     }
 
-    operatorStack.push(token)
+    stack1.push(token)
   }
 
-  while (operatorStack.length > 0) {
-    const c = operatorStack.pop()
-    postOrder.push(c)
-  }
+  while (stack1.length > 0) postOrder.push(stack1.pop())
 
   // 3. output
-  for (let x of postOrder) {
-    if (isOperator(x)) x = cal(outputStack.pop(), outputStack.pop(), x)
+  for (let token of postOrder) {
+    if (isOperator(token)) token = cal(stack2.pop(), stack2.pop(), token)
 
-    outputStack.push(x)
+    stack2.push(token)
   }
 
-  return outputStack[0]
+  return stack2[0]
+}
+
+// 2. ------------------------------------------------------------
+// 方法：因為只有四則運算，可直接運算
+// Runtime: 53.17% / 108 ms
+// Memory Usage: 28.03% / 50 MB
+const calculate2 = (s) => {
+  let num = ''
+  let operator = '+'
+  const stack = []
+
+  // function
+  const isOperator = (x) => x === '+' || x === '-' || x === '*' || x === '/'
+  const isSpace = (x) => x === ' '
+  const isHighPrecedence = (x) => x === '*' || x === '/'
+  const cal = (right, left, operator) => {
+    right = parseInt(right)
+    left = parseInt(left)
+
+    if (operator === '+') return left + right
+    if (operator === '-') return left - right
+    if (operator === '*') return left * right
+    if (operator === '/') return Math.trunc(left / right)
+  }
+
+  for (const char of s) {
+    if (isSpace(char)) continue
+
+    if (isOperator(char)) {
+      let preNum = 0
+
+      if (isHighPrecedence(operator)) preNum = stack.pop()
+
+      stack.push(cal(num, preNum, operator))
+      num = ''
+      operator = char
+
+      continue
+    }
+
+    num = num + char
+  }
+
+  while (stack.length > 0) {
+    const preNum = stack.pop()
+    num = cal(num, preNum, operator)
+    operator = '+'
+  }
+
+  return num
 }
