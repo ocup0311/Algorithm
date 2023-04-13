fcc4c137d2cbee07b3f3c2e25bde990b8171b146
Ocup <ocup0311@gmail.com>
2023-04-13 18:14:58 +0800

    [UPDATE][LEETCODE][ARRAY][EASY] #5_singleNumber

--- code/Array/@LeetCode/easy/#5_singleNumber.js
+++ code/Array/@LeetCode/easy/#5_singleNumber.js
@@ -32,7 +32,7 @@
 // 1. ------------------------------------------------------------
 // Runtime: 85.86% / 74 ms
 // Memory: 14.56% / 48.2 MB
-const singleNumber = (nums) => {
+const singleNumber1 = (nums) => {
   const single = {}
   const twice = {}
 
@@ -44,4 +44,49 @@ const singleNumber = (nums) => {
   }
 
   return Object.keys(single)[0]
+}
+
+// 二刷補充：
+// 1. ------------------------------------------------------------
+// T(n): O(n)
+// S(n): O(n)
+const singleNumber1a = (nums) => {
+  if (nums.length === 1) return nums[0]
+
+  const map1 = new Map()
+  const map2 = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map1.has(nums[i])) {
+      map1.delete(nums[i])
+      map2.set(nums[i], true)
+    } else {
+      map1.set(nums[i], true)
+    }
+  }
+
+  return map1.keys().next().value
+}
+
+// 2. ------------------------------------------------------------
+// T(n): O(nlogn)
+// S(n): O(1)
+const singleNumber2a = (nums) => {
+  if (nums.length === 1) return nums[0]
+
+  nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < nums.length; i = i + 2) {
+    if (nums[i] !== nums[i + 1]) return nums[i]
+  }
+}
+
+// 3. ------------------------------------------------------------
+// T(n): O(n)
+// S(n): O(1)
+// 使用 XOR 運算邏輯 (single 1 --> 1)
+const singleNumber3a = (nums) => {
+  if (nums.length === 1) return nums[0]
+
+  return nums.reduce((acc, cur) => acc ^ cur, 0)
 }
