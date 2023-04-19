8d2b04e8ccf1f789d93fd14a44bdda834244e4db
Ocup <ocup0311@gmail.com>
2023-04-19 19:01:58 +0800

    [UPDATE][LEETCODE][ARRAY][EASY] #8_moveZeroes

--- code/Array/@LeetCode/easy/#8_moveZeroes.js
+++ code/Array/@LeetCode/easy/#8_moveZeroes.js
@@ -30,26 +30,84 @@
 // 1. ------------------------------------------------------------
 // Runtime: 80.21% / 97 ms
 // Memory: 27.86% / 47.1 MB
-const moveZeroes = (nums) => {
-  let ptr1 = 0
-  let ptr2 = 0
+// T(n): O(n)
+// S(n): O(1)
+const solution1 = () => {
+  const moveZeroes = (nums) => {
+    let ptr1 = 0
+    let ptr2 = 0
 
-  while (ptr1 < nums.length) {
-    if (nums[ptr1] === 0) {
+    while (ptr1 < nums.length) {
+      if (nums[ptr1] === 0) {
+        ptr1++
+        continue
+      }
+
+      if (ptr1 !== ptr2) {
+        nums[ptr2] = nums[ptr1]
+      }
+
       ptr1++
-      continue
+      ptr2++
     }
 
-    if (ptr1 !== ptr2) {
-      nums[ptr2] = nums[ptr1]
+    while (ptr2 < nums.length) {
+      nums[ptr2] = 0
+      ptr2++
+    }
+  }
+}
+
+// 二刷
+// --> 以 0 的比重來看，40% 為分界，
+// --> 40% ~ 100% 選擇 solution2a
+// --> 0% ~ 40% 選擇 solution1a
+// --> 平均下來 solution2a 較划算
+// 1. ------------------------------------------------------------
+// T(n): O(n)
+// S(n): O(1)
+// 概算：6X+6Y (X: 非0  Y: 0)
+const solution1a = () => {
+  const moveZeroes = (nums) => {
+    let ptr = 0
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 0) continue
+      if (ptr !== i) nums[ptr] = nums[i]
+
+      ptr++
     }
 
-    ptr1++
-    ptr2++
+    while (ptr < nums.length) {
+      nums[ptr] = 0
+      ptr++
+    }
+
+    return
+  }
+}
+
+// 2. ------------------------------------------------------------
+// T(n): O(n)
+// S(n): O(1)
+// 概算：8X+3Y (X: 非0  Y: 0)
+const solution2a = () => {
+  const swap = (arr, idx1, idx2) => {
+    let tmp = arr[idx1]
+    arr[idx1] = arr[idx2]
+    arr[idx2] = tmp
   }
 
-  while (ptr2 < nums.length) {
-    nums[ptr2] = 0
-    ptr2++
+  const moveZeroes = (nums) => {
+    let ptr = 0
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 0) continue
+      if (ptr !== i) swap(nums, ptr, i)
+
+      ptr++
+    }
+
+    return
   }
 }
